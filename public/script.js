fce59679bcb169b774052a87964b468a548c9756
jhg <jbbillion@yahoo.com>
2021-03-27 11:15:39 -0400

    almost finished

--- public/script.js
+++ public/script.js
@@ -3,16 +3,16 @@ async function dataHandler() {
     console.log(endpoint);
     const request = await fetch(endpoint);
     const dininghalls = await request.json();
+
+    let tbody = document.getElementById("mytable");
+
+    for (let i = 0; i < dininghalls.data.length; i++) {
+        tr = document.createElement("tr");
+        tr.innerHTML = "<td>" + dininghalls.data[i].hall_name + "</td><td>" + dininghalls.data[i].hall_address + "</td><td>";
+        mytable.appendChild(tr);
+    }
+
     console.log(dininghalls)
 }
 
 dataHandler()
-
-let tbody = document.getElementById("mytable");
-tbody.appendChild(td);
-
-let td = document.createElement("td");
-td.innerHTML = "I'm table data bitch!";
-mytable.appendChild(td);
-
-for (let i = 0; i < dininghalls.length; i++) {}
